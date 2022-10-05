86708e202f8fc0fd275b4ae27d702d14c8b342c9
antedeguemon <antedeguemon@users.noreply.github.com>
2022-10-04 21:17:24 -0400

    Initial commit

---
+++ lib/type_counselor.ex
@@ -0,0 +1,18 @@
+defmodule TypeCounselor do
+  @moduledoc """
+  Documentation for `TypeCounselor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TypeCounselor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/type_counselor_test.exs
@@ -0,0 +1,8 @@
+defmodule TypeCounselorTest do
+  use ExUnit.Case
+  doctest TypeCounselor
+
+  test "greets the world" do
+    assert TypeCounselor.hello() == :world
+  end
+end
